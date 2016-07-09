30e8ee25e7a85f5eb9d7f0ec935b3cb689e469f6
Xin Liu <xliu159@hawk.iit.edu>
2016-07-08 20:08:44 -0500

    Add files via upload

---
+++ coding with C++/292. Nim Game.c
@@ -0,0 +1,37 @@
+/*
+ @ July 8 2016 
+ @ by Xin Liu 
+*/
+
+/************Description of the problem************/
+/* LeetCode 292. Nim Game
+* 
+   You are playing the following Nim Game with your friend: There is a heap of stones on the table, each time one of you take turns to remove 1 to 3 stones. 
+   The one who removes the last stone will be the winner. You will take the first turn to remove the stones.
+
+   Both of you are very clever and have optimal strategies for the game. Write a function to determine 
+    whether you can win the game given the number of stones in the heap.
+
+   For example, if there are 4 stones in the heap, then you will never win the game: no matter 1, 2, or 3 stones you remove, 
+    the last stone will always be removed by your friend.
+
+*/
+	 
+
+
+
+/**
+only when the number of stones is divisible by 4, the former player may loose the game.
+in other condition, the former player can move the stones to make the left stones is divisible by 4.
+*/
+
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+    
+        if(n%4 ==0) return false;
+        else return true;
+    }
+           
+};
